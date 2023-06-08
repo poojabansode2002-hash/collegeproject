fffdbc588e575600c08650c5dc7c0a000638e004
poojabansode2002-hash <106476102+poojabansode2002-hash@users.noreply.github.com>
2023-06-08 15:30:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,394 @@
+var express = require("express");
+var app = express();
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb+srv://pooja:pooja@cluster0.c4obztp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+const dbName = "test";
+
+const path = require('path');
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+const session = require('express-session');
+
+
+
+app.use(
+  session({
+    secret: 'your-secret-key', // Specify a secret key to sign the session ID cookie
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/home.html");
+});
+
+app.get("/signup", function(request, response) {
+    response.sendFile("http://localhost:3000");
+});
+
+app.get("/signin", function(request, response) {
+    var username = request.query.username;
+    var password = request.query.aadhar;
+    console.log(username, password);
+    async function run() {
+        try {
+            await client.connect();
+            console.log("Connected correctly to server");
+            const db = client.db(dbName);
+            const col = db.collection("UserInfo");
+            const myDoc = await col.findOne({ "firstname": username });
+            if (myDoc.firstname == username && myDoc.aadhar == password) {
+                console.clear()
+                console.log("succcesssfullly logged in");
+                response.sendFile(path.join(__dirname + "/farmeraccountID.html"))
+
+            } else {
+                console.clear()
+                console.log("invallllid");
+
+            }
+        } catch (err) {
+            console.log(err.stack);
+        } finally {
+            await client.close();
+        }
+    }
+
+    run().catch(console.dir);
+
+
+});
+
+app.get("/adminsignin", function(request, response) {
+    var username = request.query.username;
+    var password = request.query.password;
+    console.log(username, password);
+    async function run() {
+        try {
+            await client.connect();
+            console.log("Connected correctly to admin server");
+            const db = client.db(dbName);
+            const col = db.collection("admin");
+            const myDoc = await col.findOne({ "firstname": username });
+            if (myDoc.fullname == username && myDoc.password == password) {
+                console.clear()
+                console.log("succcesssfullly logged in");
+                response.sendFile(path.join(__dirname + "/farmeraccountID.html"))
+
+            } else {
+                console.clear()
+                console.log("invallllid");
+
+            }
+        } catch (err) {
+            console.log(err.stack);
+        } finally {
+            await client.close();
+        }
+    }
+
+    run().catch(console.dir);
+
+
+});
+
+app.get("/getpage", function(request, response) {
+    var username = request.query.first_name;
+    var number = request.query.aadhar_card_number;
+    var question = request.query.last_name;
+    console.log("get page is working")
+
+    async function run() {
+        try {
+            await client.connect();
+            console.log("Connected correctly to server");
+            const db = client.db(dbName);
+            const col = db.collection("UserInfo");
+            let personDocument = {
+                "firstname": username,
+                "aadhar": number,
+                "lastname": question
+            }
+            console.log("inserted")
+            await col.insertOne(personDocument);
+            response.sendFile(__dirname + "/farmersaccountpage.html");
+        } catch (err) {
+            console.log(err.stack);
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+
+app.get("/items", function(request, response) {
+    var products = request.body;
+
+    console.log("items page is working")
+
+    async function run() {
+        try {
+            await client.connect();
+            console.log("Connected correctly to server for items");
+            const db = client.db(dbName);
+            const col = db.collection("items");
+            let personDocument = {
+                "products": products,
+
+            }
+            console.log("inserted the items")
+            await col.insertOne(personDocument);
+
+        } catch (err) {
+            console.log(err.stack);
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+
+app.get("/farmeraccountpage", function(request, response) {
+    var fullname = request.query.fullname;
+    var aadhar = request.query.aadhar;
+    var phone = request.query.phone;
+    var age = request.query.age;
+    var currentadd = request.query.currentadd;
+    var permanentadd = request.query.permanentadd;
+
+    console.log("farmeraccount page is working")
+
+    async function run() {
+        try {
+            await client.connect();
+            console.log("Connected correctly to server");
+            const db = client.db(dbName);
+            const col = db.collection("farmeraccount");
+            let personDocument = {
+                "fullname": fullname,
+                "aadhar": aadhar,
+                "phone": phone,
+                "age": age,
+                "currentadd": currentadd,
+                "permanentadd": permanentadd,
+            }
+            console.log("inserted")
+            await col.insertOne(personDocument);
+            response.sendFile(path.join(__dirname + "/registerSuccessful.html"))
+        } catch (err) {
+            console.log(err.stack);
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+
+app.get("/shopkeeperaccount", function(request, response) {
+    var fullname = request.query.fullname;
+    var password = request.query.password;
+    var password1 = request.query.password1;
+    
+    var phone = request.query.phone;
+    var age = request.query.age;
+    var currentadd = request.query.currentadd;
+    var permanentadd = request.query.permanentadd;
+
+    console.log("shopkeeper account page is working")
+
+    async function run() {
+        try {
+            await client.connect();
+            console.log("Connected correctly to server");
+            const db = client.db(dbName);
+            const col = db.collection("admin");
+            let personDocument = {
+                "fullname": fullname,
+                "password": password,
+                "phone": phone,
+                "age": age,
+                "currentadd": currentadd,
+                "permanentadd": permanentadd,
+            }
+            if(password === password1){
+            console.log("shopkeeperaccount inserted")
+            await col.insertOne(personDocument);
+            response.sendFile(path.join(__dirname + "/registerSuccessful.html"))}
+        } catch (err) {
+            console.log(err.stack);
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+
+app.get("/getcontact", function(request, response) {
+    var name = request.query.name;
+    var mobile = request.query.mobile;
+    var message = request.query.message;
+    console.log("get contact page is working")
+
+    async function run() {
+        try {
+            await client.connect();
+            console.log("Connected correctly to server");
+            const db = client.db(dbName);
+            const col = db.collection("contact");
+            let personDocument = {
+                "name": name,
+                "mobile": mobile,
+                "message": message
+            }
+            console.log("inserted contact info")
+            await col.insertOne(personDocument);
+        } catch (err) {
+            console.log(err.stack);
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+
+app.get("/getlinkHome", function(request, response) {
+    response.sendFile(path.join(__dirname + "/home.html"))
+});
+
+app.get("/getlinkProducts", function(request, response) {
+    response.sendFile(path.join(__dirname + "/Products.html"))
+});
+
+app.get("/getlinkContact", function(request, response) {
+    response.sendFile(path.join(__dirname + "/Contact.html"))
+});
+
+app.get("/getlinksignin", function(request, response) {
+    response.sendFile(path.join(__dirname + "/signin.html"))
+});
+
+app.get("/getlinkadmin", function(request, response) {
+    response.sendFile(path.join(__dirname + "/admin_signin.html"))
+});
+app.get("/getlinknext", function(request, response) {
+    response.sendFile(path.join(__dirname + "/producttablepage.html"))
+});
+app.get("/getlinkregister", function(request, response) {
+    response.sendFile(path.join(__dirname + "/registeration.html"))
+});
+app.get("/getlinkregisteradmin", function(request, response) {
+    response.sendFile(path.join(__dirname + "/shopkeeperaccount.html"))
+});
+
+
+app.get("/getlinkKnowmore", function(request, response) {
+    response.sendFile(path.join(__dirname + "/Introduction.html"))
+});
+
+
+app.get("/getlinkfarmeraccount", function(request, response) {
+    response.sendFile(path.join(__dirname + "/farmersaccountpage.html"))
+});
+app.get("/gettoshowmore", function(request, response) {
+    response.sendFile(path.join(__dirname + "/showmore.html"))
+});
+app.get("/toor", function(request, response) {
+    response.sendFile(path.join(__dirname + "/Toor.html"))
+});
+app.get("/jowar", function(request, response) {
+    response.sendFile(path.join(__dirname + "/Jowar.html"))
+});
+app.get("/maize", function(request, response) {
+    response.sendFile(path.join(__dirname + "/Maize.html"))
+});
+app.get("/soyabean", function(request, response) {
+    response.sendFile(path.join(__dirname + "/Soyabean.html"))
+});
+app.get("/cotton", function(request, response) {
+    response.sendFile(path.join(__dirname + "/Cotton.html"))
+});
+app.get("/turmeric", function(request, response) {
+    response.sendFile(path.join(__dirname + "/Turmeric.html"))
+});
+app.get("/wheat", function(request, response) {
+    response.sendFile(path.join(__dirname + "/Wheat.html"))
+});
+
+app.get('/work', (req, res) => {
+    
+    
+    async function run() {
+        try {
+            await client.connect();
+            console.log("Connected correctly to server");
+            const db = client.db(dbName);
+            const col = db.collection("products");
+            const work = await col.find({}).toArray();
+            res.render('work', { work });
+           
+        } catch (err) {
+            console.log(err.stack);
+        } finally {
+            await client.close();
+        }
+    }
+
+    run().catch(console.dir);
+
+    
+});
+
+var cart = []
+app.post('/add-to-cart', (req, res) => {
+    const productName = req.body.productName;
+    const price = req.body.price;
+    const stock = req.body.stock;
+    cart.push({
+      productName: productName,
+      price: price,
+      stock: stock
+    });
+  });
+
+  
+  app.get('/cart', (req, res) => {
+    res.render('cart', { cart: cart });
+  });
+
+  app.get("/cartsubmit", function(req, res) {
+    console.log('Received data by cart submit:', cart);
+    async function run() {
+        try {
+            await client.connect();
+            console.log("Connected correctly to server");
+            const db = client.db(dbName);
+            const col = db.collection("tickets");
+            let personDocument = {
+                "data": cart,
+
+            }
+            console.log("inserted")
+            await col.insertOne(personDocument);
+        } catch (err) {
+            console.log(err.stack);
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+
+
+  
+
+const port = 3000
+app.listen(process.env.PORT || port)
+console.log("Something awesome to happen at http://localhost:" + port);
